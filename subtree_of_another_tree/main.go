b09660cf404774c459f9b12cce254650a915227b
Ali Aristanov <aristanovali618@gmail.com>
2023-12-27 22:13:18 +0600

    kalgany

--- subtree_of_another_tree/main.go
+++ subtree_of_another_tree/main.go
@@ -7,20 +7,19 @@ type TreeNode struct {
 }
 
 func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
-    if root == nil || subRoot == nil {
-        return false
-    } else if kkk(root, subRoot) {
-        return true
-    }
-    return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+	if root == nil || subRoot == nil {
+		return false
+	} else if kkk(root, subRoot) {
+		return true
+	}
+	return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
 }
 
-
 func kkk(root *TreeNode, subRoot *TreeNode) bool {
-    if root == nil || subRoot == nil {
-        return root == nil && subRoot == nil
-    } else if root.Val == subRoot.Val {
-        return kkk(root.Right, subRoot.Right) && kkk(root.Left, subRoot.Left)
-    }
-    return false
+	if root == nil || subRoot == nil {
+		return root == nil && subRoot == nil
+	} else if root.Val == subRoot.Val {
+		return kkk(root.Right, subRoot.Right) && kkk(root.Left, subRoot.Left)
+	}
+	return false
 }
